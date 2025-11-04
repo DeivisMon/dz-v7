199cc2082b13f54abfa721d4b25d4d592a8e7108
DeivisMon <deividosius@yahoo.com>
2025-11-04 18:26:37 +0200

    added page transition

---
+++ src/components/utils/PageTransitions.jsx
@@ -0,0 +1,68 @@
+import { easeInOut, motion as Motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
+export default function PageTransitions({ children }) {
+  const location = useLocation();
+
+  const Animate = (variants) => {
+    return {
+      initial: "initial",
+      animate: "animate",
+      exit: "exit",
+      variants,
+    };
+  };
+
+  const oppacity = {
+    initial: { opacity: 1 },
+    animate: { opacity: 1, transition: { duration: 1 } },
+    exit: {
+      opacity: 0.8,
+      y: -40,
+      transition: { duration: 1, ease: easeInOut },
+    },
+  };
+
+  const slide = {
+    initial: { y: "100vh" },
+    animate: { y: "100vh" },
+    exit: {
+      y: 0,
+      transition: { duration: 1, delay: 0.1, ease: easeInOut },
+    },
+  };
+
+  const zoomOut = {
+    initial: { y: 0, scale: 1, opacity: 1 },
+    animate: { y: 0, scale: 1, opacity: 1 },
+    exit: {
+      y: [0, -50, 50, 100],
+      scale: 0.7,
+      opacity: 0.8,
+      transition: { duration: 1.2, ease: easeInOut },
+    },
+  };
+
+  return (
+    <div className="bg-black/90 overflow-hidden">
+      <Motion.div
+        {...Animate(slide)}
+        className="fixed top-0 left-0 bg-black min-w-full z-1 min-h-screen"
+      >
+        <span 
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl text-white">
+          {location.pathname === "/"
+            ? "index"
+            : location.pathname.split("/").pop()}
+        </span>
+      </Motion.div>
+      <Motion.div {...Animate(zoomOut)} className="relative min-h-screen">
+        <Motion.div
+          {...Animate(oppacity)}
+          className="relative flex items-center justify-center bg-black min-h-screen"
+        >
+          {children}
+        </Motion.div>
+      </Motion.div>
+    </div>
+  );
+}
